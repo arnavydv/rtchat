5319c4293a4e1c00ddd49e4ee3df7c09a92f8aa4
arnavydv <aarnavyadvv@gmail.com>
2026-06-10 12:40:55 +0530

    chatcompleted

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 import websockets
 import asyncio
 from websockets.asyncio.server import serve
-#web socket - 2 function 
-"""
-handler - how the input/ouput handles 
-main function - actually handles web socket new connection and requests made by 
-client 
-"""
+
+async def sender_task(websocket):
+    while True:
+        reply = await asyncio.to_thread(input,"Enter the reply message:")
+        await websocket.send(reply)
+        print(f"sent a reply :{reply}")
+
+async def receiver_task(websocket):
+    while True:
+        mmessage= await websocket.recv()
+        print(f"received a message{mmessage}")
 
 async def handler(websocket):
-    while True:
-        message= await websocket.recv()
-        print(f"received message:{message}")
+    #i can create task 
+    send_task=asyncio.create_task(sender_task(websocket))
+    recv_task=asyncio.create_task(receiver_task(websocket))
+    
+    #task mee we can add completion and pending 
+    completed,pending=await asyncio.wait([recv_task,send_task],return_when = asyncio.FIRST_COMPLETED)
+
 async def main():
     async with serve(handler,"",8000) as server:
         await server.serve_forever()
